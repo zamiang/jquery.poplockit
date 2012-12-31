75f6ff3e4c4cc4ed3e65d3ec4551e7bde92fc5b4
Brennan Moore <brennanmoore@gmail.com>
2012-12-31 12:21:05 -0500

    initial compile of le js

---
+++ dist/lockit.js
@@ -0,0 +1,24 @@
+/*! Lockit - v0.1.0 - 2012-12-31
+* https://github.com/zamiang/lockit-js
+* Copyright (c) 2012 Brennan Moore; Licensed MIT */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
